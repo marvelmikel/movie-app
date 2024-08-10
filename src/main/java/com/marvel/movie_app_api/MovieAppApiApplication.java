85d22d9d4f98337953b2cc21c110800bf91cc24a
marvelmikel <marvelmikel1997@gmail.com>
2024-08-10 23:49:33 +0100

    setup

---
+++ src/main/java/com/marvel/movie_app_api/MovieAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.movie_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAppApiApplication.class, args);
+	}
+
+}
